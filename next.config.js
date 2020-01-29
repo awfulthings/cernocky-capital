3b6d386cd156db519a27eee2c492fe007a090685
Jiří Dolinski <jiri.dolinski@gmail.com>
2020-01-29 21:34:57 +0100

    Export

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withFonts = require('next-fonts');
 
 module.exports = withFonts(
     {
-        exportTrailingSlash: false,
+        exportTrailingSlash: true,
         webpack: (config, options) => {
             return config
         },
